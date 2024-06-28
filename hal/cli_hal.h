b1152c8af12fae8736a4fe34aa1efd8671381ba4
MorroMaker <linwei.idea@outlook.com>
2024-06-28 08:46:59 +0800

    Create cli_hal.h

---
+++ hal/cli_hal.h
@@ -0,0 +1,52 @@
+/**
+  * @file    cli_hal.h
+  * @author  MorroMaker
+  * @brief   Header for cli_hal.c file
+  * @attention
+  *
+  * Copyright (c) 2024 MorroMaker
+  * All rights reserved.
+  *
+  * Licensed under the Apache License, Version 2.0 (the "License");
+  * you may not use this file except in compliance with the License.
+  * You may obtain a copy of the License at
+  *
+  *     http://www.apache.org/licenses/LICENSE-2.0
+  *
+  * Unless required by applicable law or agreed to in writing, software
+  * distributed under the License is distributed on an "AS IS" BASIS,
+  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  * See the License for the specific language governing permissions and
+  * limitations under the License.
+  */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef OMNI_CLI_HAL_H
+#define OMNI_CLI_HAL_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "device.h"
+#include "common.h"
+
+#include "shell.h"
+#include "log.h"
+
+#define cli_s       shell_def
+
+/**
+ * @brief CLI handle structure
+ */
+typedef struct {
+    struct cli_s cli;
+    void (*init_callback)();
+} cli_t;
+
+#ifdef __cplusplus
+}
+#endif /* __cplusplus */
+
+#endif /* OMNI_CLI_HAL_H */
