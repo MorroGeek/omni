431885349e0bbd1357d13cf435b89cc7a6ee1b22
MorroMaker <linwei.idea@outlook.com>
2024-07-08 15:16:42 +0800

    Update the CMake architecture for APM32 platform files in targets.

--- targets/apm/apm32f4/target.h
+++ targets/apm/apm32f4/target.h
@@ -28,6 +28,49 @@
 extern "C" {
 #endif
 
+/* Includes ------------------------------------------------------------------*/
+#include "apm32f4xx_dal.h"
+#include "apm32f4xx_ddl_gpio.h"
+
+/* Exported defines ----------------------------------------------------------*/
+/* Exported macro ------------------------------------------------------------*/
+/* Exported typedef ----------------------------------------------------------*/
+
+/**
+ * @brief  Port name type definition
+ */
+typedef enum {
+    PORTA = 0x00,
+    PORTB = 0x10,
+    PORTC = 0x20,
+    PORTD = 0x30,
+    PORTE = 0x40,
+    PORTF = 0x50,
+    PORTG = 0x60,
+    PORTH = 0x70,
+    PORTI = 0x80,
+    PORTJ = 0x90,
+    PORTK = 0xA0,
+} port_name_t;
+
+struct serial_s {
+    UART_HandleTypeDef *handle;
+    USART_TypeDef *ins;
+    uint32_t max_speed;   // Maximum speed (Unit: Kbps)
+};
+
+struct i2c_s {
+    I2C_HandleTypeDef *handle;
+    I2C_TypeDef *ins;
+    uint32_t max_freq;   // Maximum frequency (Unit: KHz)
+};
+
+struct spi_s {
+    SPI_HandleTypeDef *handle;
+    SPI_TypeDef *ins;
+    uint32_t max_freq;   // Maximum frequency (Unit: KHz)
+};
+
 #ifdef __cplusplus
 }
 #endif
